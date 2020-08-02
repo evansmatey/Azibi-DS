8f10d7319ba4f22f4c85972fa1b28444f8b70014
evansmatey <68758817+evansmatey@users.noreply.github.com>
2020-08-02 01:43:46 -0700

    Update time_tracker.py

--- time_tracker.py
+++ time_tracker.py
@@ -47,36 +47,34 @@ def time_tracker(starttime, endtime, filename, extension, wageperhour = 5):
 
     print('you worked for {} and earned {}{:.2f}{}'.format(duration,'$',total_earned,'dollars'))     #using the .string format method
 
-    save_time_tracker_details(eyear, emon, eday, ehour, emin, syear, smon, sday, shour,
+    #save_time_tracker_details(eyear, emon, eday, ehour, emin, syear, smon, sday, shour,
 
                               smin , wageperhour, total_earned, duration, filename, extension)
     
     
-   
+def save_time_tracker_details( eyear, emon, eday, ehour, emin, syear, smon, sday, shour,
+                              smin , wageperhour, totalwage, totaltime, filename, extension):
 
+    """
+    @save_time_tracker_details is a simple function that just saves the data generated form the time_tracker fuction 
+    into a csv or excel file,  
+    """
+    dataname = "".join([filename, extension])     #joining the filename and the extension
+    col = np.array(['eyear', 'emon', 'eday', 'ehour', 'emin', 'syear', 'smon', 'sday', 'shour',
+                              'smin' , 'wageperhour', 'totalwage', 'time_worked'])    #creating an array
+    tracker_data = pd.DataFrame(data=[[eyear, emon, eday, ehour, emin,syear, smon, sday, shour,
+                              smin , wageperhour, totalwage, totaltime]], columns=col)     #creating the dataframe 
 
+    if extension == '.csv' or '.CSV':      #saving the dataframe as a csv or excel
 
-
-
-
-
-
-
-
-
-
-
-
-
+        tracker_data.to_csv(dataname)    
+    else:
+        tracker_data.to_excel(dataname)
 
 
 def main():
 
-    '''
-
-    @Evans if you still want to use the user input in the code add it here, if you are done delete this commet
-
-    '''
+    
     start = 2020,7,30,8,20
 
     end =  2020,7,31,12,30
@@ -101,56 +99,6 @@ if __name__ == "__main__":
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// we will be removing this function subject to confirmation from Evans
-import datetime
-import time 
-import csv
-f = open('people.csv','a', newline = '')
-start = tuple(map(int, (input('Enter the start date of the task in this order;Year,Month,Day,Hour,Minute: ').split(','))))
-end = tuple(map(int, (input('Enter the end date of the task in this order;Year,Month,Day,Hour,Minute: ').split(','))))
-def time_tracker(starttime, endtime, wageperhour = 5):
-    '''
-    @time_tracker is a function that can be used to calculate the money earned when working.
-    this function accepts tuple objects nothing else in the form '9/15/18/10/30' month/day/year/hour/minute in this 
-    order nothing else
-    '''
-    eyear, emon, eday, ehour, emin = endtime     #tuple unpacking the arguments 
-    syear, smon, sday, shour, smin = starttime
-    
-    st = datetime.datetime(syear, smon, sday, shour, smin)     #convert the stings into datetime object
-    et = datetime.datetime(eyear, emon, eday, ehour, emin) 
-    # calculating the differences between the start time and the end time 2
-    wt =  et - st 
-    divider = 3600
-    wts = wt.total_seconds()     # converting the total time worked into seconds for eazy calculation
-    ttw = int(float(str(wts)))     # convert the total time worked in seconds into integer
-    hr = ttw // divider             # convert to hours
-    total_earned = float((wts/divider)* wageperhour)     #calculating the money earned and converting the value into a float
-    print('You worked for {} hours and earned {}{:.2f}{}'.format(hr,'$',total_earned,'dollars'))     #using the .string format method
-    return hr, total_earned
-end_result = time_tracker(start, end)
-writer =csv.writer(f)
-writer.writerow(['Hours_worked', 'Salary'])
-writer.writerow(end_result)
-
-f.close()
 
 
 
